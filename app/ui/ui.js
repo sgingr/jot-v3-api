1df618ce1348c594baeece3bec8e7a84f7131ebc
Steven Gingras <steven.gingras@gmail.com>
2024-01-08 20:29:30 -0600

    missed hiddenInd

--- app/ui/ui.js
+++ app/ui/ui.js
@@ -20,7 +20,7 @@ class UI extends UiBase {
 
   async updateCategory(userId, parms) {
     let self = this;
-    await self.dao.updateCategory(parms.categoryId, parms.name, parms.icon, parms.description);
+    await self.dao.updateCategory(parms.categoryId, parms.name, parms.icon, parms.description, parms.hiddenInd);
     return await self.dao.getCategories(userId);
   }
 
